4df7c32da23517837df112923aa5dfe9716ad286
Elias <elikapeluschnik@gmail.com>
2021-11-22 16:44:35 -0500

    commit tree e2d665acfdecdfe881098bc6e6e40b5fa5e19ec5

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet "Naureen"
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    num1 + num2
+end
+
+def halve(number) 
+  if (typeof number !== "number") 
+    return nill;
+ number / 2;
+end
